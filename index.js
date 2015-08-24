feb3b724bffb774e17bebd366d2a6e7177bb904d
yanweiqing <yanweiqing@meituan.com>
2015-08-24 14:45:46 +0800

    init & add setClass

---
+++ index.js
@@ -0,0 +1,7 @@
+import { setClass } from './src/setClass';
+
+class CatUtil {};
+
+CatUtil.setClass = setClass;
+
+export default CatUtil;
---
+++ src/setClass.js
@@ -0,0 +1,9 @@
+export function setClass(classNames) {
+    if (typeof classNames === 'object') {
+        return Object.keys(classNames).filter(function(className) {
+            return classNames[className];
+        }).join(' ');
+    } else {
+        return Array.prototype.join.call(arguments, ' ');
+    }
+}
