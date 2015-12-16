7927315fb6a8cd1880cbb95c84ebb615bb40e438
yanweiqing <yanweiqing@meituan.com>
2015-12-16 16:57:24 +0800

    添加key

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     setClass: require('./dist/set-class'),
     createFragment: require('./dist/create-fragment'),
+    keyCode: require('./dist/key-code'),
 	Animate: require('./dist/animate')
 };
